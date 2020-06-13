a9ec7e91995d2a98f30953bb47d6e89bcd88ec12
andrewmniu <andrewmniu@gmail.com>
2020-06-13 03:30:33 -0400

    Updates

---
+++ precache-manifest.6c2c453a9f032389d0048de4adb781bd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d424ea8b03b14a65af3137607786859",
+    "url": "/spotify-stats/index.html"
+  },
+  {
+    "revision": "76222cc6e05140435e17",
+    "url": "/spotify-stats/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "9ce99e5a296607a159a1",
+    "url": "/spotify-stats/static/css/main.854097f3.chunk.css"
+  },
+  {
+    "revision": "76222cc6e05140435e17",
+    "url": "/spotify-stats/static/js/2.243954b4.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/spotify-stats/static/js/2.243954b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ce99e5a296607a159a1",
+    "url": "/spotify-stats/static/js/main.2272f2da.chunk.js"
+  },
+  {
+    "revision": "60111d87ff58fe8e6ff2",
+    "url": "/spotify-stats/static/js/runtime-main.6497f044.js"
+  },
+  {
+    "revision": "69ac66699e15312b55d96cac2ed8e20e",
+    "url": "/spotify-stats/static/media/spotify-theme-music-bkg-dark.69ac6669.png"
+  }
+]);

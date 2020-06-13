d4a0ff775d78176153196332221b52e133989b5f
andrewmniu <andrewmniu@gmail.com>
2020-06-13 03:18:09 -0400

    changed redirect_uri

--- backend/app.js
+++ backend/app.js
@@ -16,7 +16,7 @@ require('dotenv').config();
 
 var client_id = process.env.CLIENT_ID; // Your client id
 var client_secret = process.env.CLIENT_SECRET; // Your secret
-var redirect_uri = "http://localhost:8888/callback"; // Your redirect uri
+var redirect_uri =  "https://peaceful-fortress-73033.herokuapp.com/callback"// "http://localhost:8888/callback"; // Your redirect uri
 
 /**
  * Generates a random string containing numbers and letters
